557a6485260ec27f8626ff47e0b26918fd6f138a
flexino-is-here <aavipul11@gmail.com>
2021-01-11 22:39:30 +0530

    MY FIRST APP

---
+++ MYAPP.cs
@@ -0,0 +1,14 @@
+using system;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("Hello World!!")
+            Console.WriteLine("I am VIPUL GUPTA and this is my 1st App using C# and .net SDK 5.0.101")
+            Console.WriteLine("LHD DAY 1")
+        }
+    }
+}
